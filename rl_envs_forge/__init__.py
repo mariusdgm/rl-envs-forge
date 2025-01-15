4a3548f0511d6f9c81afd4ef1e188160c939f03a
Marius Dragomir <marius.dragomir.dgm@gmail.com>
2025-01-15 23:24:27 +0200

    metadata version (#59)

--- rl_envs_forge/__init__.py
+++ rl_envs_forge/__init__.py
@@ -1,11 +1,3 @@
-import toml
-import os
+from importlib.metadata import version
 
-# Define the path to the pyproject.toml
-current_init_file_path = os.path.dirname(__file__)
-pyproject_path = os.path.join(current_init_file_path, "..", "pyproject.toml")
-
-# Read the version from the pyproject.toml
-with open(pyproject_path, "r") as f:
-    pyproject_data = toml.load(f)
-    __version__ = pyproject_data["tool"]["poetry"]["version"]
+__version__ = version("rl-envs-forge")
